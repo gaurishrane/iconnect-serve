f428fe0a676d3f3d5007e0dafd135b67d9a2ed78
Gaurish <megaurishrane@gmail.com>
2020-04-30 20:07:02 +0530

    master

--- utils/connection.js
+++ utils/connection.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 
 exports.connect = async() => {
     var connectionData = databaseSettings[config.connectionName]
-    var connectionString = `mongodb://${config.host}:${config.dbPort}/${config.mongoDB}`;
+        // var connectionString = `mongodb://${config.host}:${config.dbPort}/${config.mongoDB}`;
+    var connectionString = `"mongodb+srv://cluster0-nkx5o.mongodb.net/test" --username admin`
 
     return await mongoose.connect(connectionString, {
         useNewUrlParser: true,
